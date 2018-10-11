9c46b2d6f17696da22e67570219970eae7e47b67
FRED <f.turiot@gmail.com>
2018-10-11 19:15:12 +0200

    ajout de la rooute

--- BoVoyageMVC/Controllers/FrontContactMessagesController.cs
+++ BoVoyageMVC/Controllers/FrontContactMessagesController.cs
@@ -36,6 +36,7 @@ namespace BoVoyageMVC.Controllers
         }
 
         // GET: ContactMessages/Create
+        [Route("Nous-contacter")]
         public ActionResult Create()
         {
             return View();
