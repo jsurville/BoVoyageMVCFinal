2306dbec8773101e3ec7a5018eb99214997d411f
sertoi <nguyentuongvien@yahoo.com>
2018-10-11 09:12:33 +0200

    Date retour > date Aller,Ajouter Format display

--- BoVoyageMVC/Validators/DateAttribute.cs
+++ BoVoyageMVC/Validators/DateAttribute.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace BoVoyDateMVC.Validators
+namespace BoVoyageMVC.Validators
 {
     [AttributeUsage(AttributeTargets.Property)] 
     public class DateAttribute : ValidationAttribute
---
+++ BoVoyageMVC/Validators/DecimalAttibute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageMVC.Validators
+{
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DecimalAtribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                if (value is String) return (value.ToString().Length) > 0;
+
+                if (value is int n) return n >= 0;
+                if (value is decimal d) return d > 0.0m;
+            }
+            else
+            {
+                
+                throw new ArgumentException("Le valeur doit être positible ");
+            }
+            return false;
+        }
+       
+    }
+
+}
+       
+   
