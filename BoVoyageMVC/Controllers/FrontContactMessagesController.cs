62c915bd611d78049ca2a1aa77dc615618022eb0
FRED <f.turiot@gmail.com>
2018-10-12 14:56:58 +0200

    nous contacter avec retout sur homme     controleur commenté

--- BoVoyageMVC/Controllers/FrontContactMessagesController.cs
+++ BoVoyageMVC/Controllers/FrontContactMessagesController.cs
@@ -14,26 +14,26 @@ namespace BoVoyageMVC.Controllers
     {
         
 
-        // GET: ContactMessages
-        public ActionResult Index()
-        {
-            return View(db.ContactMessages.ToList());
-        }
+        //// GET: ContactMessages
+        //public ActionResult Index()
+        //{
+        //    return View(db.ContactMessages.ToList());
+        //}
 
-        // GET: ContactMessages/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ContactMessage contactMessage = db.ContactMessages.Find(id);
-            if (contactMessage == null)
-            {
-                return HttpNotFound();
-            }
-            return View(contactMessage);
-        }
+        //// GET: ContactMessages/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    ContactMessage contactMessage = db.ContactMessages.Find(id);
+        //    if (contactMessage == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(contactMessage);
+        //}
 
         // GET: ContactMessages/Create
         //[Route("Nous-contacter")]
@@ -55,68 +55,68 @@ namespace BoVoyageMVC.Controllers
             {
                 db.ContactMessages.Add(contactMessage);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", "Home");
             }
 
             return View(contactMessage);
         }
 
         // GET: ContactMessages/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ContactMessage contactMessage = db.ContactMessages.Find(id);
-            if (contactMessage == null)
-            {
-                return HttpNotFound();
-            }
-            return View(contactMessage);
-        }
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    ContactMessage contactMessage = db.ContactMessages.Find(id);
+        //    if (contactMessage == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(contactMessage);
+        //}
 
         // POST: ContactMessages/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Title,LastName,FisrtName,Email,PhoneNumber,SendDate,Message")] ContactMessage contactMessage)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(contactMessage).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(contactMessage);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,Title,LastName,FisrtName,Email,PhoneNumber,SendDate,Message")] ContactMessage contactMessage)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(contactMessage).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(contactMessage);
+        //}
 
-        // GET: ContactMessages/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ContactMessage contactMessage = db.ContactMessages.Find(id);
-            if (contactMessage == null)
-            {
-                return HttpNotFound();
-            }
-            return View(contactMessage);
-        }
+        //// GET: ContactMessages/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    ContactMessage contactMessage = db.ContactMessages.Find(id);
+        //    if (contactMessage == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(contactMessage);
+        //}
 
-        // POST: ContactMessages/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            ContactMessage contactMessage = db.ContactMessages.Find(id);
-            db.ContactMessages.Remove(contactMessage);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// POST: ContactMessages/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    ContactMessage contactMessage = db.ContactMessages.Find(id);
+        //    db.ContactMessages.Remove(contactMessage);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
         protected override void Dispose(bool disposing)
         {
