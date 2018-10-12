1a9f7a46c0785c611555aa3ababb5f433bb4157d
jsurville <julien.surville@gmail.com>
2018-10-12 12:23:36 +0200

    Validation DossierReservations BackOffice en cours

---
+++ BoVoyageMVC/Services/CarteBancaireService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyageMVC.Services
+{
+    class CarteBancaireService
+    {
+        public bool ValiderSolvabilite(string numeroCarteBancaire, double prixTotal)
+        {
+            double solde = 0;
+            double.TryParse(numeroCarteBancaire, out solde);
+            return (solde > prixTotal);  
+        }
+
+        public bool Rembourser(string numeroCarteBancaire, decimal prixTotal)
+        {
+            return true ;
+        }
+
+        public bool PayerAgence(object p)
+        {
+            return true ;
+        }
+    }
+}
